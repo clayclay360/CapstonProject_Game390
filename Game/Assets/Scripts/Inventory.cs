4d5700191599b120e3491d9f91bf1acae1500d20
SamsoGitHub <112575840+SamsoGitHub@users.noreply.github.com>
2022-09-24 20:33:55 -0400

    Commit #4
    -added a test throw knife prefab
    -added code to throw_knife function, but might do some changes.

--- Game/Assets/Scripts/Inventory.cs
+++ Game/Assets/Scripts/Inventory.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    [Header("References")]
+    public Transform cam;
+    public Transform attackPoint;
+    public GameObject objectToThrow;
+
+    [Header("Settings")]
+    public int totalThrows;
+    public float throwCooldown;
+
+    [Header("Throwing")]
+    public KeyCode throwKey = KeyCode.F;
+    public float throwForce;
+    public float throwUpwardForce;
+
+    bool readyToThrow;
 
     //Holds item ids for held items
     int main_hand_id;
@@ -19,6 +34,8 @@ public class Inventory : MonoBehaviour
         off_hand_id = 0;
 
         hands_full = false;
+
+        readyToThrow = true;
     }
 
     // Update is called once per frame
@@ -36,6 +53,7 @@ public class Inventory : MonoBehaviour
                 throw_knife();
             }
         }
+
     }
 
     //if hands are not full, pick up item
@@ -83,6 +101,37 @@ public class Inventory : MonoBehaviour
     //placeholder knife throw function
     public void throw_knife()
     {
-        
+        readyToThrow = false;
+
+        // instantiate object to throw
+        GameObject projectile = Instantiate(objectToThrow, attackPoint.position, cam.rotation);
+
+        // get rigidbody component
+        Rigidbody projectileRb = projectile.GetComponent<Rigidbody>();
+
+        // calculate direction
+        Vector3 forceDirection = cam.transform.forward;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(cam.position, cam.forward, out hit, 500f))
+        {
+            forceDirection = (hit.point - attackPoint.position).normalized;
+        }
+
+        // add force
+        Vector3 forceToAdd = forceDirection * throwForce + transform.up * throwUpwardForce;
+
+        projectileRb.AddForce(forceToAdd, ForceMode.Impulse);
+
+        totalThrows--;
+
+        // implement throwCooldown
+        Invoke(nameof(ResetThrow), throwCooldown);
+    }
+
+    private void ResetThrow()
+    {
+        readyToThrow = true;
     }
 }
