0e10f9428424f9e008868c7e6461939ad28e8808
SamsoGitHub <112575840+SamsoGitHub@users.noreply.github.com>
2022-10-02 21:33:19 -0400

    commit #8
    -added a spawner script, but it does have an error which the prefab will be out of bound, so will get fix

---
+++ Game/Assets/Scripts/Spawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public Transform[] spawnLocation;
+    public GameObject[] foodPrefab;
+    public GameObject[] foodClone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawn();
+    }
+
+    void spawn()
+    {
+        foodClone[0] = Instantiate(foodPrefab[0], spawnLocation[0].transform.position, Quaternion.Euler(0, 0, 0));
+        foodClone[1] = Instantiate(foodPrefab[1], spawnLocation[1].transform.position, Quaternion.Euler(0, 0, 0));
+        foodClone[2] = Instantiate(foodPrefab[1], spawnLocation[2].transform.position, Quaternion.Euler(0, 0, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
