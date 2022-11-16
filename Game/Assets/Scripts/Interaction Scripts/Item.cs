36ee9c75492a6778e3a026e495bc4136af1dc571
LostOneironaut <sbrownst@gmail.com>
2022-11-16 15:26:29 -0500

    Item Respawn Should Work
    I may have missed calling itemrespawn somewhere but I think I got them all.

--- Game/Assets/Scripts/Interaction Scripts/Item.cs
+++ Game/Assets/Scripts/Interaction Scripts/Item.cs
@@ -107,6 +107,8 @@ public class Item : MonoBehaviour, IInteractable<PlayerController.ItemInMainHand
         {
             meshRenderer.enabled = true;
         }
+        transform.position.Set(startPosition.x, startPosition.y, startPosition.z);
+        transform.rotation.Set(startRotation.x, startRotation.y, startRotation.z, startRotation.w);
     }
 
     public void CheckCounter()
