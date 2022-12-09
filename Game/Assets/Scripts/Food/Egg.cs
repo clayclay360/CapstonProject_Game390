a65244f591caaff436a1f7f3ad9f7bbc920b9dc5
OwenJauch13 <112638008+OwenJauch13@users.noreply.github.com>
2022-12-09 14:11:49 -0500

    Small fix for nullreference exception

--- Game/Assets/Scripts/Food/Egg.cs
+++ Game/Assets/Scripts/Food/Egg.cs
@@ -28,8 +28,8 @@ public class Egg : Item
     {
         base.Start();
         passItems = GameObject.Find("PassItems");
-        GameManager.egg = GameObject.Find("Egg(Clone)").GetComponent<Egg>();
         origPos = transform.position;
+        StartCoroutine(WaitOneSecond());
     }
 
     public void Update()
@@ -166,5 +166,12 @@ public class Egg : Item
     {
         transform.position = origPos;
         gameObject.SetActive(true);
+    }
+
+    private IEnumerator WaitOneSecond()
+    {
+        yield return new WaitForSeconds(1);
+        GameManager.egg = GameObject.Find("Egg(Clone)").GetComponent<Egg>();
+        //Debug.Log(GameManager.egg);
     }
 }
